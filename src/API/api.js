638be6eb4570d18588060ad2a1464cad9ac35319
Roozbeh <150560157+rzbeh@users.noreply.github.com>
2024-09-12 21:50:09 +0330

    Update api.js

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5120";
+const API_BASE_URL = "https://localhost:7076";
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 300000,
